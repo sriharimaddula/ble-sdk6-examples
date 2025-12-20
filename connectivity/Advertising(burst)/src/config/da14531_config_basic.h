a4b09239e996391bf191a6b3fc077a59b67b25b5
sriharimaddula <96724710+sriharimaddula@users.noreply.github.com>
2025-12-20 06:04:47 +0000

    Reduce code size: Disable UART logging, Debug mode, and DISS profile

--- connectivity/Advertising(burst)/src/config/da14531_config_basic.h
+++ connectivity/Advertising(burst)/src/config/da14531_config_basic.h
@@ -82,14 +82,14 @@
 /*      -   Enables Debug module and sets code execution in breakpoint in Hardfault and NMI (Watchdog) handlers.*/
 /*          It allows developer to hot attach debugger and get debug information                                */
 /****************************************************************************************************************/
-#define CFG_DEVELOPMENT_DEBUG
+// #define CFG_DEVELOPMENT_DEBUG
 
 /****************************************************************************************************************/
 /* UART Console Print. If CFG_PRINTF is defined, serial interface logging mechanism will be enabled.            */
 /* If CFG_PRINTF_UART2 is defined, then serial interface logging mechanism is implented using UART2, else UART1 */
 /* will be used.                                                                                                */
 /****************************************************************************************************************/
-#define CFG_PRINTF
+// #define CFG_PRINTF
 #ifdef CFG_PRINTF
     #define CFG_PRINTF_UART2
 #endif
